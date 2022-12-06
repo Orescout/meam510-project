c33a5863674906bd57cd9967e4c884daf7517301
Orescout <orestis.skoutellas@gmail.com>
2022-12-05 19:12:08 -0500

    getting close to parsing the JSON

--- project/body.h
+++ project/body.h
@@ -11,10 +11,16 @@ const char body[] PROGMEM = R"===(
       color: rgb(235, 235, 235)
     }
 
+    .canvas {
+      position: absolute;
+      left: 0px;
+      top: 400px;
+    }
+
     .arrowbtn {
       position: absolute;
-      width: 100px;
-      height: 100px;
+      width: 50px;
+      height: 50px;
       background: rgba(17, 17, 17, 0);
       border: 2px solid hotpink;
       border-radius: 100px;
@@ -35,18 +41,20 @@ const char body[] PROGMEM = R"===(
       position: absolute;
       display: inline-block;
       content: "";
-      width: 25px;
-      height: 25px;
-      top: 50%;
-      left: 50%;
+      width: 10px;
+      height: 10px;
+      top: 65%;
+      left: 60%;
     }
 
     /* ~~~~~~~~ LOOK UP/DOWN/LEFT/RIGHT CSS ~~~~~~~~~~~~ */
 
-    .arrowbtn-lookup { /* UP */
+    .arrowbtn-lookup {
+      /* UP */
       left: 720px;
       top: 20px;
     }
+
     .arrowbtn-lookup:after {
       margin-left: -12.5px;
       margin-top: -6.25px;
@@ -55,10 +63,12 @@ const char body[] PROGMEM = R"===(
       transform: rotateZ(45deg);
     }
 
-    .arrowbtn-lookdown { /* DOWN */
+    .arrowbtn-lookdown {
+      /* DOWN */
       left: 720px;
       top: 200px;
     }
+
     .arrowbtn-lookdown:after {
       margin-left: -12.5px;
       margin-top: -18.75px;
@@ -67,10 +77,12 @@ const char body[] PROGMEM = R"===(
       transform: rotateZ(45deg);
     }
 
-    .arrowbtn-lookleft { /* LEFT */
+    .arrowbtn-lookleft {
+      /* LEFT */
       top: 100px;
-      left: 590px;
+      left: 400px;
     }
+
     .arrowbtn-lookleft:after {
       margin-left: -8.5px;
       margin-top: -13.75px;
@@ -79,10 +91,12 @@ const char body[] PROGMEM = R"===(
       transform: rotateZ(135deg);
     }
 
-    .arrowbtn-lookright { /* RIGHT */
+    .arrowbtn-lookright {
+      /* RIGHT */
       top: 100px;
-      left: 850px;
+      left: 500px;
     }
+
     .arrowbtn-lookright:after {
       margin-left: -17.5px;
       margin-top: -13.75px;
@@ -93,10 +107,12 @@ const char body[] PROGMEM = R"===(
 
     /* ~~~~~~~~ MOVE UP/DOWN/LEFT/RIGHT CSS ~~~~~~~~~~~~ */
 
-    .arrowbtn-moveup { /* UP */
+    .arrowbtn-moveup {
+      /* UP */
       left: 720px;
-      top: 420px;
+      top: 20px;
     }
+
     .arrowbtn-moveup:after {
       margin-left: -12.5px;
       margin-top: -6.25px;
@@ -105,10 +121,12 @@ const char body[] PROGMEM = R"===(
       transform: rotateZ(45deg);
     }
 
-    .arrowbtn-movedown { /* DOWN */
+    .arrowbtn-movedown {
+      /* DOWN */
       left: 720px;
-      top: 600px;
+      top: 100px;
     }
+
     .arrowbtn-movedown:after {
       margin-left: -12.5px;
       margin-top: -18.75px;
@@ -117,11 +135,13 @@ const char body[] PROGMEM = R"===(
       transform: rotateZ(45deg);
     }
 
-    .arrowbtn-moveleft { /* LEFT */
-      top: 500px;
-      left: 590px;
+    .arrowbtn-moveleft {
+      /* LEFT */
+      top: 60px;
+      left: 650px;
     }
-    .arrowbtn-moveleft:after { 
+
+    .arrowbtn-moveleft:after {
       margin-left: -8.5px;
       margin-top: -13.75px;
       border-bottom: 2px solid;
@@ -129,10 +149,12 @@ const char body[] PROGMEM = R"===(
       transform: rotateZ(135deg);
     }
 
-    .arrowbtn-moveright { /* RIGHT */
-      top: 500px;
-      left: 850px;
+    .arrowbtn-moveright {
+      /* RIGHT */
+      top: 60px;
+      left: 790px;
     }
+
     .arrowbtn-moveright:after {
       margin-left: -17.5px;
       margin-top: -13.75px;
@@ -151,8 +173,11 @@ const char body[] PROGMEM = R"===(
   <b>Key Pressed: </b>
   <span id="last_key_pressed_text"> </span> <br>
 
-  <b>Server Response: </b>
+  <b>Server Response from keys pressed: </b>
   <span id="response_text"> </span> <br>
+
+  <b>Raw JSON from ESP response: </b>
+  <span id="response_JSON"> </span> <br>
 
   <span class="arrowbtn arrowbtn-lookleft" id="arrow-lookleft"></span>
   <span class="arrowbtn arrowbtn-lookright" id="arrow-lookright"></span>
@@ -161,6 +186,11 @@ const char body[] PROGMEM = R"===(
   <span class="arrowbtn arrowbtn-movedown" id="arrow-movedown"></span>
   <span class="arrowbtn arrowbtn-moveleft" id="arrow-moveleft"></span>
   <span class="arrowbtn arrowbtn-moveright" id="arrow-moveright"></span>
+
+  <canvas class="canvas" id="canvas-background" width="705" height="285" style="border:1px solid #ffffff;"></canvas>
+  <canvas class="canvas" id="canvas-robot" width="705" height="285"></canvas>
+  <canvas class="canvas" id="canvas-IR-vision" width="705" height="285"></canvas>
+
 </body>
 
 <script>
@@ -190,6 +220,7 @@ const char body[] PROGMEM = R"===(
 
     xhttp.open("GET", url, true);
     xhttp.send();
+
   }
 
   var map = {}; // You could also use an array
@@ -208,7 +239,7 @@ const char body[] PROGMEM = R"===(
     // Look
     toggleColor("arrow-lookleft", map[37]); // ArrowLeft
     toggleColor("arrow-lookright", map[39]); // ArrowRight
-    
+
     // Move
     toggleColor("arrow-moveup", map[87]); // W
     toggleColor("arrow-movedown", map[83]); // S
@@ -219,14 +250,154 @@ const char body[] PROGMEM = R"===(
     if (map[48] || map[49] || map[50] || map[51] || map[52] || map[53] || map[54] || map[55] || map[56] || map[57]) {
       setSpeed(e.key);
     }
+  }
 
-    /* Turbo Mode
-    if (map[16]) {
-      document.style.background = "rgb(255, 51, 51)";
+  const scale_multiplier = 2.5;
+  var robot_width = 50;
+  var robot_height = 50;
+  var canvas_already_setup = false;
+  var delay_per_update_request = 1000;
+
+  // Declare drawing objects
+  var context_background = document.getElementById("canvas-background").getContext("2d");
+  var context_robot = document.getElementById("canvas-robot").getContext("2d");
+
+  function drawCanvasBackground() {
+    var h = document.getElementById("canvas-background").clientHeight;
+    var w = document.getElementById("canvas-background").clientWidth;
+
+    context_background.fillStyle = "#003366";
+    context_background.fillRect(0, 0, w / 2, h); // draw our side
+
+    context_background.fillStyle = "#660033";
+    context_background.fillRect(w / 2, 0, w / 2, h); // draw opposite side
+
+    context_background.strokeStyle = "rgba(0, 0, 0, 0.8)";
+    context_background.strokeRect(w / 4, h / 5, w / 8, h * (3 / 5)); // left 2x points box
+    context_background.strokeRect(w * (7.5 / 12), h / 5, w / 8, h * (3 / 5)); // right 2x points box
+
+  }
+
+  // Repeat asking for updated state from ESP
+  let repeating_state_ask = setInterval(updateState, delay_per_update_request);
+
+  // Ask for updated state from ESP
+  function updateState() {
+    let response = "";
+    let ESP_response = JSON.parse("{}");
+    var xhttp = new XMLHttpRequest();
+    var url = "get_updated_state";
+
+    xhttp.onreadystatechange = function () {
+      if (this.readyState == 4 && this.status == 200) {
+        response = this.responseText;
+        //response = response.replaceAll("'", "\"");
+        response = response.replaceAll(" ", "");
+        ESP_response = JSON.stringify(response);
+        ESP_response = JSON.parse(ESP_response);
+        console.log(ESP_response);
+        console.log(ESP_response.success);
+        console.log(ESP_response.setup.game_width);
+      }
+    };
+
+    xhttp.open("GET", url, true);
+    xhttp.send();
+
+    
+    if (!canvas_already_setup && !ESP_response.skip_setup) {
+      // Perform setup - done once. We use the multiplier to go from cm to desired pixel sizes
+
+      document.getElementById("canvas-background").width = ESP_response.setup.game_width * scale_multiplier;
+      document.getElementById("canvas-background").height = ESP_response.setup.game_height * scale_multiplier;
+
+      document.getElementById("canvas-robot").width = ESP_response.setup.game_width * scale_multiplier;
+      document.getElementById("canvas-robot").height = ESP_response.setup.game_height * scale_multiplier;
+
+      robot_width = ESP_response.setup.robot_width * scale_multiplier;
+      robot_height = ESP_response.setup.robot_height * scale_multiplier;
+
+      delay_per_update_request = ESP_response.setup.delay_per_update_request;
+
+      drawCanvasBackground();
+
+      canvas_already_setup = true;
+
+    } else if (ESP_response.status == "success") {
+      // update the state of the robot, by using the multiplier to go from cm to desired pixel sizes
+      updateRobotState(ESP_response.robot.x * scale_multiplier, ESP_response.robot.y * scale_multiplier, ESP_response.robot.degrees,
+        ESP_response.IR_sensor.beacon_700Hz, ESP_response.IR_sensor.beacon_32Hz,
+        ESP_response.ToF_sensor.distance * scale_multiplier,
+        ESP_response.motors.power.front_left_A, ESP_response.motors.power.back_left_B, ESP_response.motors.power.front_right_C, ESP_response.motors.power.back_right_D,
+        ESP_response.motors.direction.front_left_A, ESP_response.motors.direction.back_left_B, ESP_response.motors.direction.front_right_C, ESP_response.motors.direction.back_right_D);
+
     } else {
-      document.style.background = "rgb(35, 35, 35)";
+      console.log("ERROR: NO RESPONSE FROM ESP32 RECEIVED!!!");
     }
-    */
+  }
+
+  //moveRobot(100, 100, 110, 
+  //              1, 1,
+  //              3, 
+  //              1.0, 1.0, 1.0, 0.3,
+  //              1, 1, 1, 0);
+
+  function updateRobotState(x_coordinate, y_coordinate, degrees_facing, see_700Hz_beacon, see_32Hz_beacon, distance,
+    motor_power_A, motor_power_B, motor_power_C, motor_power_D,
+    motor_dir_A, motor_dir_B, motor_dir_C, motor_dir_D) {
+    // reset canvas
+    context_robot.setTransform(1, 0, 0, 1, 0, 0); // Use the identity matrix while clearing the canvas
+    context_robot.clearRect(0, 0, document.getElementById("canvas-background").clientWidth, document.getElementById("canvas-background").clientHeight); // Clean it
+
+    var offset_degrees = 135; // Our starting point is top left which is 10.30 o'clock, whereas the cos/sin clock starts from 3 o'clock. That's a difference of 135 degrees.
+    var radius = Math.sqrt((robot_width / 2) ** 2 + (robot_height / 2) ** 2);
+
+    // Get new coordinates of top left corner, after displacement rcos(theta) and rsin(theta)
+    var top_left_corner_x_coordinates = x_coordinate + radius * Math.cos((degrees_facing - offset_degrees) * Math.PI / 180);
+    var top_left_corner_y_coordinates = y_coordinate + radius * Math.sin((degrees_facing - offset_degrees) * Math.PI / 180);
+
+    // Draw robot location rectangle
+    context_robot.fillStyle = "rgba(255, 255, 255, 0.6)";
+    context_robot.translate(top_left_corner_x_coordinates, top_left_corner_y_coordinates);
+    context_robot.rotate(degrees_facing * Math.PI / 180);
+    context_robot.fillRect(0, 0, robot_width, robot_height);
+
+    // Draw robot direction triangle
+    context_robot.fillStyle = "rgba(255, 255, 255, 0.8)";
+    context_robot.beginPath();
+    context_robot.moveTo(robot_width * 0.2, robot_width / 5);
+    context_robot.lineTo(robot_width * 0.5, 0);
+    context_robot.lineTo(robot_width * 0.8, robot_width / 5);
+    context_robot.fill();
+
+    // Draw where robot sees with Infrared Receiver 700Hz
+    (see_700Hz_beacon) ? context_robot.fillStyle = "rgba(255, 255, 0, 0.6)" : context_robot.fillStyle = "rgba(255, 255, 0, 0.1)";
+    context_robot.beginPath();
+    context_robot.moveTo(robot_width * 0.2, 0);
+    context_robot.lineTo(robot_width * 0.4, -document.getElementById("canvas-background").clientWidth);
+    context_robot.lineTo(robot_width * 0.5, 0);
+    context_robot.fill();
+
+    // Draw where robot sees with Infrared Receiver 32Hz
+    (see_32Hz_beacon) ? context_robot.fillStyle = "rgba(0, 255, 0, 0.6)" : context_robot.fillStyle = "rgba(0, 255, 0, 0.1)";
+    context_robot.beginPath();
+    context_robot.moveTo(robot_width * 0.8, 0);
+    context_robot.lineTo(robot_width * 0.6, -document.getElementById("canvas-background").clientWidth);
+    context_robot.lineTo(robot_width * 0.5, 0);
+    context_robot.fill();
+
+    // Draw power of wheels
+    context_robot.fillStyle = "rgba(" + motor_dir_A * 255 + ", " + motor_dir_A * 255 + ", " + motor_dir_A * 255 + "," + motor_power_A + ")"; // Front Left Motor A
+    context_robot.fillRect(robot_width * 0.05, robot_height * 0.1, robot_width * 0.1, robot_height * 0.3);
+
+    context_robot.fillStyle = "rgba(" + motor_dir_B * 255 + ", " + motor_dir_B * 255 + ", " + motor_dir_B * 255 + "," + motor_power_B + ")"; // Back Left Motor B
+    context_robot.fillRect(robot_width * 0.05, robot_height * 0.6, robot_width * 0.1, robot_height * 0.3);
+
+    context_robot.fillStyle = "rgba(" + motor_dir_C * 255 + ", " + motor_dir_C * 255 + ", " + motor_dir_C * 255 + "," + motor_power_C + ")"; // Front Right Motor C
+    context_robot.fillRect(robot_width * 0.85, robot_height * 0.1, robot_width * 0.1, robot_height * 0.3);
+
+    context_robot.fillStyle = "rgba(" + motor_dir_D * 255 + ", " + motor_dir_D * 255 + ", " + motor_dir_D * 255 + "," + motor_power_D + ")"; // Back Right Motor D
+    context_robot.fillRect(robot_width * 0.85, robot_height * 0.6, robot_width * 0.1, robot_height * 0.3);
   }
 
 
